7a9a26222d07a859b7eebd0088faa7a9fd3a74a1
mohagitesd <mohabousy@gmail.com>
2025-03-03 11:55:02 +0100

    feat: create shortlink

--- src/Controller/ShortlinkController.php
+++ src/Controller/ShortlinkController.php
@@ -1,18 +1,52 @@
 <?php
 
 namespace App\Controller;
-
+use App\DTO\CreateShortLinkDTO;
+use App\Service\ShortLinkService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ShortlinkController extends AbstractController
+
+final class ShortLinkController extends AbstractController
+
+
 {
-    #[Route('/shortlink', name: 'app_shortlink')]
-    public function index(): Response
+
+
+    #[Route('/short-links', methods: ['POST'], name: 'create_short_link')]
+
+
+    public function create(
+
+
+        #[MapRequestPayload]
+
+
+        CreateShortLinkDTO $dto,
+
+
+        ShortLinkService $shortLinkService,
+
+
+    ): Response
+
+
     {
-        return $this->render('shortlink/index.html.twig', [
-            'controller_name' => 'ShortlinkController',
-        ]);
+
+
+        $shortLink = $shortLinkService->createShortLink($dto);
+
+
+
+
+
+        return $this->json($shortLink);
+
+
     }
+
+
 }
+
